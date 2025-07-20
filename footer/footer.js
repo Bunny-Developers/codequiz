f6c246c5b68729013377f53b32fb751284e7a72c
Hillary Okoth <hillaryokoth002@gmail.com>
2025-07-21 02:12:20 +0300

    organize and make folders

---
+++ footer/footer.js
@@ -0,0 +1,12 @@
+// Function to load footer
+function loadFooter() {
+  fetch('/footer/index.html')
+    .then(response => response.text())
+    .then(data => {
+      document.body.insertAdjacentHTML('beforeend', data);
+    })
+    .catch(error => console.error('Error loading footer:', error));
+}
+
+// Call the function to load footer
+loadFooter();
