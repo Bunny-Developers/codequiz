55fa9eebf39b4fca8972db65a1f86d2a90a38718
Hillary Okoth <hillaryokoth002@gmail.com>
2026-02-01 20:12:07 +0300

    initial commit

--- test/game.js
+++ test/game.js
@@ -14,7 +14,7 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 // Fetch questions from local JSON file
-fetch('/questions.json')
+fetch('https://bunny-developers.github.io/codequiz/questions.json')
   .then((res) => {
     if (!res.ok) {
       throw new Error('Network response was not ok');
@@ -60,7 +60,7 @@ startGame = () => {
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score);
-    return window.location.assign('/end.html'); // Go to the end page
+    return window.location.assign('https://bunny-developers.github.io/codequiz/end.html'); // Go to the end page
   }
 
   questionCounter++;
